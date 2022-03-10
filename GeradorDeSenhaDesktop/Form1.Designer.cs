13ac607a947382eec0155df915d8bca96de835f7
victor.andrade <victor.andrade@RAOCSCTIFRO011>
2022-03-10 14:57:46 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorDeSenhaDesktop/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace GeradorDeSenhaDesktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbTamanhoSenha = new System.Windows.Forms.TextBox();
+            this.txbSenhaGerada = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tamanho da senha: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txbTamanhoSenha
+            // 
+            this.txbTamanhoSenha.Location = new System.Drawing.Point(130, 20);
+            this.txbTamanhoSenha.Name = "txbTamanhoSenha";
+            this.txbTamanhoSenha.Size = new System.Drawing.Size(100, 23);
+            this.txbTamanhoSenha.TabIndex = 2;
+            this.txbTamanhoSenha.Text = "9";
+            this.txbTamanhoSenha.TextChanged += new System.EventHandler(this.txbTamanhoSenha_TextChanged);
+            // 
+            // txbSenhaGerada
+            // 
+            this.txbSenhaGerada.Location = new System.Drawing.Point(130, 131);
+            this.txbSenhaGerada.Name = "txbSenhaGerada";
+            this.txbSenhaGerada.Size = new System.Drawing.Size(100, 23);
+            this.txbSenhaGerada.TabIndex = 3;
+            this.txbSenhaGerada.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Senha gerada:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Location = new System.Drawing.Point(258, 131);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(75, 23);
+            this.btnCopiar.TabIndex = 5;
+            this.btnCopiar.Text = "Copiar";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(12, 49);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(133, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Caractere especial:   ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.checkBox2.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(12, 74);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(133, 19);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Numero:                    ";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Gerar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(352, 167);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnCopiar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbSenhaGerada);
+            this.Controls.Add(this.txbTamanhoSenha);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Segoe UI Symbol", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GERA SENHA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox txbTamanhoSenha;
+        private TextBox txbSenhaGerada;
+        private Label label2;
+        private Button btnCopiar;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+        private Button button1;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ GeradorDeSenhaDesktop/Form1.cs
@@ -0,0 +1,169 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
+// 
+// H
+// W
+// P
+// 
+
+namespace GeradorDeSenhaDesktop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(txbSenhaGerada.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txbTamanhoSenha_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txbSenhaGerada_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string tamanhoSenha = txbTamanhoSenha.Text;
+
+            if (Regex.IsMatch(tamanhoSenha, @"^[0-9]+$"))// Se o numero estivero vazio
+            {
+                int qnt = int.Parse(txbTamanhoSenha.Text);
+
+                string abcPossiveis = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower();
+
+                string numPossiveis = "123456789";
+
+                string caracteresPossiveis = "!@#$%";
+
+                Random random = new Random();
+                int numeroRandom = 0;
+                int numeroRandom2 = 0;
+                StringBuilder passwordABC = new StringBuilder();
+                StringBuilder passwordNUM = new StringBuilder();
+                StringBuilder passwordCarac = new StringBuilder();
+
+                if (checkBox1.Checked == true) //Caractere Especial marcado
+                {
+                    for (int t = 0; t < (1); t++)
+                    {
+                        numeroRandom = random.Next(0, caracteresPossiveis.Length - 1);
+                        passwordCarac.Append(caracteresPossiveis[numeroRandom]);
+                    }
+
+                    if (checkBox2.Checked == true) //Caractere e Numero marcado
+                    {
+                        for (int t = 0; t < (qnt / 2); t++)
+                        {
+                            numeroRandom = random.Next(0, abcPossiveis.Length - 1);
+                            passwordABC.Append(abcPossiveis[numeroRandom]);
+                            numeroRandom2 = random.Next(0, numPossiveis.Length - 1);
+                            passwordNUM.Append(numPossiveis[numeroRandom2]);
+                        }
+                    }
+                    else //Caractere marcado e Numero desmarcado
+                    {
+                        for (int t = 0; t < (qnt - 1); t++)
+                        {
+                            numeroRandom = random.Next(0, abcPossiveis.Length - 1);
+                            passwordABC.Append(abcPossiveis[numeroRandom]);
+                        }
+                    }
+                }
+                else  //Caractere desmarcado
+                {
+                    if (checkBox2.Checked == true) //Caractere desmarcado e Numero marcado
+                    {
+                        int converteTcbTamSenha = int.Parse(txbTamanhoSenha.Text);
+                        if (converteTcbTamSenha % 2 == 0) // Se for par
+                        {
+                            for (int t = 0; t < (qnt / 2); t++)
+                            {
+                                numeroRandom = random.Next(0, abcPossiveis.Length - 1);
+                                passwordABC.Append(abcPossiveis[numeroRandom]);
+                            }
+                            for (int t = 0; t < (qnt / 2); t++)
+                            {
+                                numeroRandom2 = random.Next(0, numPossiveis.Length - 1);
+                                passwordNUM.Append(numPossiveis[numeroRandom2]);
+                            }
+                        }
+                        else // Se for impar (adiciona uma letra a mais)
+                        {
+                            for (int t = 0; t < (qnt / 2 + 1); t++)
+                            {
+                                numeroRandom = random.Next(0, abcPossiveis.Length - 1);
+                                passwordABC.Append(abcPossiveis[numeroRandom]);
+                            }
+                            for (int t = 0; t < (qnt / 2); t++)
+                            {
+                                numeroRandom2 = random.Next(0, numPossiveis.Length - 1);
+                                passwordNUM.Append(numPossiveis[numeroRandom2]);
+                            }
+                        }
+
+                    }
+                    else //Caractere e Numero desmarcado
+                    {
+                        for (int t = 0; t < (qnt); t++)
+                        {
+                            numeroRandom = random.Next(0, abcPossiveis.Length - 1);
+                            passwordABC.Append(abcPossiveis[numeroRandom]);
+                        }
+                    }
+                }
+
+                string password = $"{passwordABC}{passwordNUM}{passwordCarac}";
+
+                txbSenhaGerada.Text = password.ToString();
+                errorProvider1.SetError(txbTamanhoSenha, "");
+            }
+            else
+            {
+                errorProvider1.SetError(txbTamanhoSenha, "Atenção!");
+                
+            }
+
+        }
+    }
+}
